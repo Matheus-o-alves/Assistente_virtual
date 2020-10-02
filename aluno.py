88b0154022d3221f844e41aa347b354dc9fca253
Matheus <65368831+Matheus-o-alves@users.noreply.github.com>
2020-10-01 22:05:16 -0300

    Delete aluno.py

--- aluno.py
+++
@@ -1,9 +0,0 @@
-print("Programa para calcular media dos alunos")
-alunos = int(input("Quantos alunos"))
-for nome in alunos:
-	nome(alunos) = string (input("\n primeiro aluno : "))
-	nota(alunos) = int(input("\n nome do aluno : "))	
-for notas in alunos:
-	print(" nome : {} " .format(nome))
-	print("\n nota : {}" .format(notas))
-
